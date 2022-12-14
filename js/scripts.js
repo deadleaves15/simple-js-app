55cd8f2f6fe6295e1d454fc5fa9dad6b596fa7c5
deadleaves15 <69413633+deadleaves15@users.noreply.github.com>
2022-12-13 17:54:18 -0700

    This commit will establish my Pokemon List with several objects and values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-alert('Hello world');
-
-let favoriteFood = 'Hamburger';
-document.write(favoriteFood);
-
-
+let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+        {name: 'Charmander', height: 0.6, type: 'Fire'},
+        {name: 'Squirtle', height: 0.5, type: 'Water'},
+        {name: 'Snorlax', height: 2.1, type: 'Normal'},
+        {name: 'Onix', height: 8.8, type: ['Rock', 'Ground']},
+        {name: 'Arbok', height: 3.5, type: 'Poison'},
+        {name: 'Pikachu', height: 0.4, type: 'Electric'},
+    ];
 
