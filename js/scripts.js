5e8a3002a38acc35f227e4da778a7a6c58e0dbc8
deadleaves15 <69413633+deadleaves15@users.noreply.github.com>
2022-12-27 18:49:49 -0700

    This commit should wrap pokemonList an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-let pokemonList = [
-        {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-        {name: 'Charmander', height: 0.6, type: 'Fire'},
-        {name: 'Squirtle', height: 0.5, type: 'Water'},
-        {name: 'Snorlax', height: 2.1, type: 'Normal'},
-        {name: 'Onix', height: 8.8, type: ['Rock', 'Ground']},
-        {name: 'Arbok', height: 3.5, type: 'Poison'},
-        {name: 'Pikachu', height: 0.4, type: 'Electric'},
-    ];
+let pokemonRepository = (function () {
+  let pokemonList = [
+  {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+  {name: 'Charmander', height: 0.6, type: 'Fire'},
+  {name: 'Squirtle', height: 0.5, type: 'Water'},
+  {name: 'Snorlax', height: 2.1, type: 'Normal'},
+  {name: 'Onix', height: 8.8, type: ['Rock', 'Ground']},
+  {name: 'Arbok', height: 3.5, type: 'Poison'},
+  {name: 'Pikachu', height: 0.4, type: 'Electric'}];
 
-/* for (let i=0; i<pokemonList.length; i++) {
-  if (pokemonList[i].height >= 2.0){
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-  else {
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>")
-  }
-}*/
 
-pokemonList.forEach(function(user) {
-  document.write(user.name + ' is ' + user.height + ' meters.' + "<br>");
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(pokemon.name + ' is ' + pokemon.height + ' meters.' + "<br>");
 });
